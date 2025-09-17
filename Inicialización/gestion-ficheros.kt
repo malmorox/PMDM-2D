6f7e31213e2a557c6a3c9f73afc72f6c95f06ba6
Marcos Almorox <73890028+malmorox@users.noreply.github.com>
2025-09-17 11:26:45 +0200

    Update gestion-ficheros.kt

--- Inicialización/gestion-ficheros.kt
+++ Inicialización/gestion-ficheros.kt
@@ -1,1 +1,21 @@
+import java.io.File
+import java.io.IOException
 
+fun main() {
+    val ruta = "prueba.txt"
+    var archivo = File(ruta)
+    var texto = "hola\nq tal\nestas"
+
+    try {
+        val bw = archivo.bufferedWriter()
+        bw.write(texto)
+        bw.close()
+        println("Archivo creado con exito")
+
+        val br = archivo.bufferedReader().use { it.readText() }
+        println(br)
+        archivo.bufferedWriter().close()
+    } catch (e: IOException) {
+        println("Error al cargar el archivo: ${e.message}")
+    }
+}
